a42e5d392affbb29e9c02e3f1c673203854d369d
Rahul Saini <rahul545436@gmail.com>
2026-07-25 19:02:43 +0530

    singleton

---
+++ patterns/Singleton.java
@@ -0,0 +1,64 @@
+package patterns;
+
+public class Singleton {
+
+  public static void main(String[] args) {
+    var obj = SingletonWithStaticExample.getInstance();
+    var obj2 = SingletonWithDLC.getInstance();
+
+    System.out.println(obj.value);
+    System.out.println(obj2.value);
+
+    EnumSingleton.INSTANCE.doSomething();
+  }
+
+}
+
+// holder idiom singleton implementation
+class SingletonWithStaticExample {
+  public final String value = "Hello World";
+
+  private SingletonWithStaticExample() {
+  }
+
+  private static class Holder {
+    private static final SingletonWithStaticExample INSTANCE = new SingletonWithStaticExample();
+  }
+
+  public static SingletonWithStaticExample getInstance() {
+    return Holder.INSTANCE;
+  }
+
+}
+
+// DLC (Double-Checked Locking) Singleton implementation
+class SingletonWithDLC {
+
+  public final String value = "Hello World";
+
+  private SingletonWithDLC() {
+  }
+
+  private volatile static SingletonWithDLC instance;
+
+  public static SingletonWithDLC getInstance() {
+    if (instance == null) {
+      synchronized (SingletonWithDLC.class) {
+        if (instance == null) {
+          instance = new SingletonWithDLC();
+        }
+      }
+    }
+    return instance;
+  }
+}
+
+// best implementation in java is using enum, because it handles serialization
+// and reflection attacks
+enum EnumSingleton {
+  INSTANCE;
+
+  public void doSomething() {
+    System.out.println("Doing something");
+  }
+}
